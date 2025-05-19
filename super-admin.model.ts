dac8bb56ffa3315b4973a3a3671e12d33a7a36d6
daya <daya.disolutions@gmail.com>
2025-05-19 15:37:15 +0530

    - Remove the user model and add super-admin model.

--- super-admin.model.ts
+++ super-admin.model.ts
@@ -1,41 +1,60 @@
 import { Sequelize, Model, DataTypes } from 'sequelize';
 
-export class User extends Model {
+export class SuperAdmin extends Model {
     public id!: string;
-    public name!: string;
+    public first_name!: string;
+    public middle_name!: string;
+    public last_name!: string;
     public email!: string;
     public password!: string;
-    public mobile_no!: string;
+    public phone!: string;
+    public role_id!: string;
+    public is_active!: boolean;
 
     static initModel(connection: Sequelize) {
-        User.init(
+        SuperAdmin.init(
             {
                 id: {
                     type: new DataTypes.UUID(),
                     defaultValue: DataTypes.UUIDV4,
                     primaryKey: true,
                 },
-                name: {
+                first_name: {
+                    type: new DataTypes.STRING(255),
+                    allowNull: false,
+                },
+                middle_name: {
+                    type: new DataTypes.STRING(255),
+                    allowNull: false,
+                },
+                last_name: {
                     type: new DataTypes.STRING(255),
                     allowNull: false,
                 },
                 email: {
                     type: new DataTypes.STRING(255),
                     allowNull: false,
-                    defaultValue: true,
                 },
                 password: {
                     type: new DataTypes.STRING(255),
                     allowNull: false,
-                    defaultValue: false,
                 },
-                mobile_no: {
+                phone: {
                     type: new DataTypes.STRING(20),
-                    allowNull: true,
+                    allowNull: false,
+                },
+                role_id: {
+                    type: new DataTypes.UUID,
+                    allowNull: false,
+                },
+                is_active: {
+                    type: new DataTypes.BOOLEAN,
+                    allowNull: false,
+                    defaultValue: true
                 }
             },
             {
-                tableName: 'user',
+                tableName: 'super_admin',
                 sequelize: connection,
                 freezeTableName: true,
                 timestamps: true,  // adds createdAt and updatedAt
